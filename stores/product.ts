f499b7900673e3526a3ef2f96001dc9df70be5f2
Dicki Maulana <bukandicki@gmail.com>
2024-10-10 01:28:35 +0700

    improve fetch all products with query params

--- stores/product.ts
+++ stores/product.ts
@@ -1,12 +1,16 @@
-import type { ProductCategoryType, ProductDataResponse } from "~/lib/types"
+import { PRODUCT_FIELDS } from "~/lib/constants"
+import type { ProductCategoryType, ProductDataResponse, ProductType } from "~/lib/types"
 
 export const useProductStore = defineStore("product", () => {
   const { public: { baseApi } } = useRuntimeConfig()
 
+  const productDetail = ref<ProductType | null>(null)
   const productData = ref<ProductDataResponse | null>(null)
   const categories = ref<ProductCategoryType[] | null>([])
 
-  async function fetchCategories() {
+  const productFieldNames = PRODUCT_FIELDS.map(f => f.name).join(",")
+
+  async function fetchCategories(): Promise<void> {
     const { data } = await useFetch<
       ProductCategoryType[]
     >(`${baseApi}/products/categories?limit=5`)
@@ -14,26 +18,38 @@ export const useProductStore = defineStore("product", () => {
     categories.value = data.value
   }
 
-  async function fetchProducts() {
-    const fields = [
-      "id",
-      "title",
-      "price",
-      "description",
-      "reviews",
-      "category",
-      "thumbnail",
-      "availabilityStatus",
-      "rating",
-      "discountPercentage"
-    ]
+  async function fetchProducts({ category, sortBy, limit }: {
+    category?: string;
+    sortBy?: { field: string; order: "asc" | "desc" };
+    limit?: number;
+  }): Promise<void> {
+    let path = `${baseApi}/products`
+    let queryParams = [`select=${productFieldNames}`]
 
-    const { data } = await useFetch<
-      ProductDataResponse
-    >(`${baseApi}/products?limit=12&select=${fields.join(",")}`)
+    if (category && category !== "all") path += `/category/${category}`
+    if (limit) queryParams.push(`limit=${limit}`)
+    if (sortBy) queryParams.push(`sortBy=${sortBy.field}&order=${sortBy.order}`)
+    if (queryParams.length) path += `?${queryParams.join("&")}`
+
+    const { data } = await useFetch<ProductDataResponse>(path)
 
     productData.value = data.value
   }
 
-  return { productData, categories, fetchCategories, fetchProducts }
+  async function fetchProductDetail(id: number): Promise<void> {
+    const { data } = await useFetch<
+      ProductDataResponse
+    >(`${baseApi}/products/${id}?select=${productFieldNames}`)
+
+    productData.value = data.value
+  }
+
+  return {
+    productData,
+    categories,
+    productDetail,
+    fetchCategories,
+    fetchProducts,
+    fetchProductDetail
+  }
 })
