02c54196e2619262bc5f1c03ac8e6ac5cbdd080f
Dicki Maulana <bukandicki@gmail.com>
2024-10-09 15:58:36 +0700

    create types lib

---
+++ lib/types/index.ts
@@ -0,0 +1,27 @@
+export type ProductCategoryType = {
+  slug: string;
+  name: string;
+  url: string;
+}
+
+export type ProductType = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  thumbnail: string;
+  availabilityStatus: string;
+  discountPercentage: string;
+  rating: number;
+}
+
+export type ProductPagination = {
+  total: number;
+  skip: number;
+  limit?: number;
+}
+
+export type ProductDataResponse = {
+  products: ProductType[];
+} & ProductPagination
