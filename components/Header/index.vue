cd2f7d778a011e5b11c49d8dd152098e184202ca
Dicki Maulana <bukandicki@gmail.com>
2024-10-09 20:20:08 +0700

    style: create header styling

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -1,7 +1,47 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import CartIcon from "~/assets/icons/cart-icon.vue"
+  import HeartIcon from "~/assets/icons/heart-icon.vue"
+</script>
 
 <template>
-  <header class="Header"></header>
+  <header class="Header">
+    <NuxtLink href="/">
+      <NuxtImg
+        src="/images/evermos-logo.png"
+        class="Header__brand"
+      />
+    </NuxtLink>
+
+    <nav class="Header__nav">
+      <ul class="Nav__list Nav__list--center">
+        <li class="Nav__item">
+          <button>About</button>
+        </li>
+        <li class="Nav__item">
+          <button>Membership</button>
+        </li>
+        <li class="Nav__item">
+          <button>Community</button>
+        </li>
+        <li class="Nav__item">
+          <button>Blog</button>
+        </li>
+      </ul>
+
+      <ul class="Nav__list Nav__list--right">
+        <li class="Nav__item">
+          <button>
+            <HeartIcon width="22" height="22" />
+          </button>
+        </li>
+        <li class="Nav__item">
+          <button>
+            <CartIcon width="22" height="22" />
+          </button>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <style lang="scss">
