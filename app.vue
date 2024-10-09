b6886a0c55723273c457d93987520a56b7329d1e
Dicki Maulana <bukandicki@gmail.com>
2024-10-09 17:57:44 +0700

    init header & footer component

--- app.vue
+++ app.vue
@@ -10,6 +10,8 @@
   <NuxtRouteAnnouncer />
 
   <NuxtLayout>
+    <LazyHeader />
     <NuxtPage />
+    <LazyFooter />
   </NuxtLayout>
 </template>
---
+++ components/Header/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="Header"></header>
+</template>
+
+<style lang="scss">
+  @import "./Header.styles.scss";
+</style>
