94feac8b36420e5a8c5bf7cebfe06775f9596010
Dicki Maulana <bukandicki@gmail.com>
2024-10-09 11:35:55 +0700

    chore: enable SSR in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
+  modules: ['@pinia/nuxt', '@nuxt/image', '@nuxt/fonts'],
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt', '@nuxt/image', '@nuxt/fonts']
 })
