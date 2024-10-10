34e0f31acf61b1b8a9cad2eec0143b543b71acdf
Dicki Maulana <bukandicki@gmail.com>
2024-10-10 22:31:54 +0700

    finishing animated text component

--- components/AnimatedText/index.vue
+++ components/AnimatedText/index.vue
@@ -1,10 +1,50 @@
 <script setup lang="ts">
+  import gsap from 'gsap';
+  import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+  if (typeof window !== "undefined") gsap.registerPlugin(ScrollTrigger)
+
+  const props = defineProps<{ value: string }>()
+
+  const splitedText = computed(() => {
+    return props.value.split(/(\s+)/)
+  })
+
+  onMounted(() => {
+    ScrollTrigger.batch(".AnimatedText", {
+      once: true,
+      onEnter: (elements) => {
+        elements.forEach((container, idx) => {
+          const wordElements = container.querySelectorAll(".AnimatedText__word")
+
+          gsap.to(wordElements, {
+            y: 0,
+            delay: 0.080 * idx,
+            duration: .85,
+            ease: "power2.out"
+          })
+        })
+      }
+    })
+  })
 </script>
 
 <template>
-  <div class="AnimatedText">
+  <span class="AnimatedText">
+    <template
+      v-for="(text, idx) in splitedText"
+      :key="idx"
+    >
+      <span
+        v-if="text !== ' '"
+        class="AnimatedText__mask"
+      >
+        <span class="AnimatedText__word">{{ text }}</span>
+      </span>
 
-  </div>
+      <template v-else>{{ " " }}</template>
+    </template>
+  </span>
 </template>
 
 <style lang="scss">
